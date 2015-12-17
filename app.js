34ec06b5281b4016f7fe2c5c5b010a451ca5fdd6
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-12-17 07:09:11 -0500

    added forecast

--- app.js
+++ app.js
@@ -18,11 +18,43 @@ weatherApp.config(function ($routeProvider) {
     
 });
 
-// Controller
-weatherApp.controller('homeContoller', ['$scope', function($scope) {
+// Services
+weatherApp.service('cityService', function() {
+    
+    this.city = "New York, NY";
+    
+})
 
+// Controllers
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+  
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
 }]);
 
-weatherApp.controller("forecastContoller", ['$scope', function($scope) {
+weatherApp.controller("forecastController", ['$scope', '$resource', 'cityService', function($scope, $resource, cityService) {
 
+    $scope.city = cityService.city;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=29d2e4711bc2829de4459406bdb6c915", { callback: "JSON_CALLBACK" }, { get: {method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: 2});
+    
+    console.log($scope.weatherResult);
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) {
+     
+        return new Date(dt * 1000);
+        
+    }
+    
 }]);
