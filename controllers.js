5c5fbb76cc39d9c55b487e51bebe2b538f0280ec
merlinoa88@gmail.com <merlinoa88@gmail.com>
2015-12-19 12:56:08 -0500

    finished course

---
+++ controllers.js
@@ -0,0 +1,41 @@
+// Controllers
+weatherApp.controller('homeController', ['$scope', '$location', 'cityService', function($scope, $location, cityService) {
+  
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function() {
+        cityService.city = $scope.city;
+    });
+    
+    $scope.submit = function() {
+        $location.path("/forecast");
+    }
+}]);
+
+
+
+weatherApp.controller("forecastController", ['$scope', '$resource', '$routeParams', 'cityService', function($scope, $resource, $routeParams, cityService) {
+
+    $scope.city = cityService.city;
+    
+    $scope.days = $routeParams.days || '2';
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast/daily?APPID=29d2e4711bc2829de4459406bdb6c915", { callback: "JSON_CALLBACK" }, { get: {method: "JSONP" }});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({ q: $scope.city, cnt: $scope.days});
+    
+    console.log($scope.weatherResult);
+    
+    $scope.convertToFahrenheit = function(degK) {
+        
+        return Math.round((1.8 * (degK - 273)) + 32);
+        
+    }
+    
+    $scope.convertToDate = function(dt) {
+     
+        return new Date(dt * 1000);
+        
+    }
+    
+}]);
